51d2feddd11816b22957510928c0009fabb54175
jhotlann <19623263@std.stei.itb.ac.id>
2025-03-23 14:27:10 +0700

    fix:bot desciption dan cmd file

--- src/alternative-bots/Toowin/Toowin.cs
+++ src/alternative-bots/Toowin/Toowin.cs
@@ -3,28 +3,17 @@ using System.Drawing;
 using Robocode.TankRoyale.BotApi;
 using Robocode.TankRoyale.BotApi.Events;
 
-// ------------------------------------------------------------------
-// 
-// ------------------------------------------------------------------
-// A sample bot original made for Robocode by Mathew Nelson.
-// Ported to Robocode Tank Royale by Flemming N. Larsen.
-//
-// This bot moves to a corner, then swings the gun back and forth.
-// If it dies, it tries a new corner in the next round.
-// ------------------------------------------------------------------
+
 public class Toowin : Bot
 {
     
-    // The main method starts our bot
     static void Main(string[] args)
     {
         new Toowin().Start();
     }
 
-    // Constructor, which loads the bot config file
     Toowin() : base(BotInfo.FromFile("Toowin.json")) { }
 
-    // Called when a new round is started -> initialize and do some movement
     public override void Run()
     {
         // Set colors
@@ -34,21 +23,15 @@ public class Toowin : Bot
         BulletColor = Color.Red;
         ScanColor = Color.Red;
 
-              
-        // Spin gun back and forth
         while (IsRunning)
         {
-            // Tell the game that when we take move, we'll also want to turn right... a lot
             SetTurnLeft(400);
-                      
-            // Start moving (and turning)
             Forward(200);
             SetTurnRight(400);
             Forward(200);
         }
     }
 
-    // We saw another bot -> stop and fire!
     public override void OnScannedBot(ScannedBotEvent e)
     {
     
@@ -62,16 +45,11 @@ public class Toowin : Bot
             Fire(3.5);
         }
         TurnToFaceTarget(e.X,e.Y);
-        // Forward(20);
     }
 
-    // Custom fire method that determines firepower based on distance.
-    // distance: The distance to the bot to fire at.
-    
 
     public override void OnHitBot(HitBotEvent e)
     {
-        // Interruptible = true;
         var bearing = GunBearingTo(e.X, e.Y);
         if (bearing >= 140 || bearing <= -140)
         {
@@ -93,31 +71,16 @@ public class Toowin : Bot
         
     }
 
-    // public override void OnHitByBullet(HitByBulletEvent e){
-    //     SetTurnRight(80);
-    //     Back(80);
-    // }
-
-    // public override void OnBulletHit(BulletHitBotEvent e){
-    //     TurnToFaceTarget(e.X,e.Y);
-    //     Fire(1);
-    // }
-    
-
     public override void OnHitWall(HitWallEvent e)
     {
         Interruptible = true;
         TurnLeft(40);
         Forward(80);
-        // Back(80);
-        // SetTurnRight(160);
-        // Back(80);
        
     }
 
     public override void OnWonRound(WonRoundEvent e)
     {
-        // Victory dance turning right 360 degrees 100 times
         TurnLeft(36_000);
     }
 
@@ -132,6 +95,5 @@ public class Toowin : Bot
         {
             TurnRight(-bearing);
         }
-        // Fire(3);
     }
 }
